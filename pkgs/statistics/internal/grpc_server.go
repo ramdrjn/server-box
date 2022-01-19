42c85a1b51eac0b12b76bc16e7253298db3da006
Ram <ramdrjn@gmail.com>
2022-01-18 21:24:08 -0500

    grpc server

--- pkgs/statistics/internal/grpc_server.go
+++ pkgs/statistics/internal/grpc_server.go
@@ -1,5 +1,30 @@
 package statistics
 
+import (
+	"net"
+	"google.golang.org/grpc"
+)
+
 func InitializeGrpcServer(stc *StatsContext) error {
-	return nil
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
