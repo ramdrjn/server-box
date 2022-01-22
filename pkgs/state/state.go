3609a3d250bd069f83fc1356a6be9642444003e2
Ram <ramdrjn@gmail.com>
2022-01-21 22:12:22 -0500

    state

--- pkgs/state/state.go
+++ pkgs/state/state.go
@@ -20,13 +20,13 @@ func Initialize(debug bool, confFilePath string) error {
 	statecontext.Log = common.InitializeLogger("state", logLevel)
 
 	//Update internal log so that its accessible everwhere
-	Log = log
+	Log = statecontext.Log
 
 	statecontext.Log.Debug("state logging initialized")
 
-	err = ProcessConfFile(confFilePath, &statecontext.Conf)
+	err = common.ProcessConfFile(confFilePath, &statecontext.Conf)
 	if err != nil {
-		statscontext.Log.Error("configuration file %s failed: ",
+		statecontext.Log.Error("configuration file %s failed: ",
                         confFilePath, err)
 		return err
 	}
